bf715707c7e36ea1f4bd7442a445346829cea8a8
Arend van Beelen jr <arendjr@gmail.com>
2022-06-29 10:59:33 +0200

    Create install-arch-packages.sh

---
+++ install-arch-packages.sh
@@ -0,0 +1,6 @@
+pacman -S \
+    gnome-keyring    # Otherwise VS code keeps complaining \
+    kmix             # To get my volume controls working \
+    mold             # For faster linking \
+    noto-fonts-emoji # To render emojis \
+    vim              # Because life
